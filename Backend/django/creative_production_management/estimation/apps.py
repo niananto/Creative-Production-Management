be3c903add5e44819708cbdebf00807bead8fd65
Fardin Anam Aungon <52667920+fardinanam@users.noreply.github.com>
2023-02-16 16:38:42 +0600

    django project initialized

---
+++ Backend/django/creative_production_management/estimation/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EstimationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'estimation'
---
+++ Backend/django/creative_production_management/estimation/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+def estimation(request):
+  return render(request, 'estimation.html')
+
